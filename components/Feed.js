a7ab1b34369d89b59707be978b21d13462d92a75
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-08-16 17:03:53 -1000

    improve social interaction buttons

--- components/Feed.js
+++ components/Feed.js
@@ -8,43 +8,47 @@ import {
   StyleSheet,
   FlatList,
 } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign"; 
+import Icon2 from "react-native-vector-icons/Fontisto"; 
+
+
 import UsersList from "./UsersList";
 import { FeedData } from "../constants";
 import { COLORS } from "../constants";
 
 const Feed = () => {
+  const PostCard = ({ post }) => (
+    <View style={styles.postCard}>
+      <View style={styles.postHeader}>
+        <Image source={post.avatar} style={styles.postAvatar} />
+        <Text style={styles.postUsername}>{post.username}</Text>
+      </View>
 
-
-const PostCard = ({ post }) => (
-  <View style={styles.postCard}>
-    <View style={styles.postHeader}>
-      <Image source={post.avatar} style={styles.postAvatar} />
-      <Text style={styles.postUsername}>{post.username}</Text>
-      <Text style={styles.postDate}>{post.date}</Text>
+      <Text style={styles.postSubtitle}>{post.subtitle}</Text>
+      <Text style={styles.postDescription}>{post.description}</Text>
+      {post.image && <Image source={post.image} style={styles.postImage} />}
+      <View style={styles.postFooter}>
+        <View style={styles.interactionButtons}>
+          <TouchableOpacity style={styles.postButton}>
+            <Icon name="like2" size={18} color={COLORS.gray} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.postButton}>
+            <Icon2 name="comment" size={18} color={COLORS.gray} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.postButton}>
+            <Icon name="sharealt" size={18} color={COLORS.gray} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.postDate}>{post.date}</Text>
+        </View>
+      </View>
     </View>
-   
-    <Text style={styles.postSubtitle}>{post.subtitle}</Text>
-    <Text style={styles.postDescription}>{post.description}</Text>
-    {post.image && <Image source={post.image} style={styles.postImage} />}
-    <View style={styles.postFooter}>
-      <TouchableOpacity style={styles.postButton}>
-        <Text style={styles.postButtonText}>Like</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.postButton}>
-        <Text style={styles.postButtonText}>Comment</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.postButtonText}>Share</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+  );
 
   return (
     <ScrollView style={styles.container}>
-   
-       <UsersList />
-  
+      <UsersList />
 
       <FlatList
         data={FeedData}
@@ -58,7 +62,7 @@ const PostCard = ({ post }) => (
 
 const styles = StyleSheet.create({
   container: {
-paddingBottom:100,
+    paddingBottom: 100,
   },
   postListContainer: {
     paddingTop: 20,
@@ -68,22 +72,30 @@ paddingBottom:100,
   postCard: {
     marginBottom: 10,
     padding: 16,
+    marginTop: 40,
     backgroundColor: COLORS.white,
     borderRadius: 5,
   },
   postHeader: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 5,
+    justifyContent: "space-between",
+    marginBottom: 10,
   },
   postAvatar: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
+    width: 60,
+    height: 60,
+    borderRadius: 50,
     marginRight: 10,
+    position: "absolute",
+    top: -46,
   },
   postUsername: {
-    flex: 1,
+    top: -10,
+    position: "absolute",
+    left: 66,
+    color: COLORS.gray,
   },
   postDate: {
     fontSize: 12,
@@ -92,10 +104,11 @@ paddingBottom:100,
   postSubtitle: {
     fontSize: 16,
     color: COLORS.black,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingVertical: 8,
+    paddingTop: 10,
   },
-  
+
   postDescription: {
     fontSize: 12,
     color: COLORS.gray,
@@ -108,6 +121,14 @@ paddingBottom:100,
   postFooter: {
     flexDirection: "row",
     marginTop: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  interactionButtons: {
+    flexDirection: "row",
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "space-around",
   },
   postButton: {
     marginRight: 10,
